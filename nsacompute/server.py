dbfaeb3a40021aa5651eeea6975ad9506e2b1f5b
waqasislam <clocksquiltobligetenseobtain612@protonmail.com>
2021-10-09 19:22:24 -0700

    new

--- nsacompute/server.py
+++ nsacompute/server.py
@@ -2,7 +2,7 @@
 import socket
 import select
 import sys
-import _thread
+from _thread import *
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 if len(sys.argv) != 3:
